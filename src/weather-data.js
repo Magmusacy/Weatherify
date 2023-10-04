f3dfeb09db664183d97df1e399d9f41e1c7de698
Magmusacy <xpfk.098@gmail.com>
2023-10-04 17:09:18 +0200

    Write function that returns weather object

---
+++ src/weather-data.js
@@ -0,0 +1,25 @@
+export function weather(location) {
+  const getWeatherData = async () => {
+    const key = "26bdff49ae6e46cfb0463609230310";
+    const url = `http://api.weatherapi.com/v1/current.json?key=${key}&q=${location}`;
+    const response = await fetch(url);
+    return await response.json();
+  };
+
+  const getDataObject = async () => {
+    const data = await getWeatherData();
+    return {
+      location: data.location.name,
+      temp_c: data.current.temp_c,
+      temp_f: data.current.temp_f,
+    };
+  };
+
+  return { getDataObject };
+}
+
+weather("cracow")
+  .getDataObject()
+  .then((data) => {
+    console.log(data);
+  });
